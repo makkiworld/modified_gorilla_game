c1b39100c5645462d57467c3be5247bd4ef22e12
makkiworld <mkh3153@g.rit.edu>
2026-09-17 09:47:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -351,12 +351,12 @@ function draw() {
 
 function drawBackgroundSky() {
   const gradient = ctx.createLinearGradient(0, 0, 0, window.innerHeight);
-  if (settings.mode === "dark") {
-    gradient.addColorStop(1, "#27507F");
-    gradient.addColorStop(0, "#58A8D8");
+   if (settings.mode === "dark") {
+    gradient.addColorStop(1, "#2E1A47");
+    gradient.addColorStop(0, "#6B2D8C");
   } else {
-    gradient.addColorStop(1, "#F8BA85");
-    gradient.addColorStop(0, "#FFC28E");
+    gradient.addColorStop(1, "#FFB6D9");
+    gradient.addColorStop(0, "#D9A7F5");
   }
   ...
 
